a1dfd6e93d2649a22867150d12f940a8a12d1631
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-01-22 23:39:36 +0530

    AddressScreen.dart

--- Advance Jewelry/lib/Screens/AddressScreen.dart
+++ Advance Jewelry/lib/Screens/AddressScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Screens/BuyPage.dart';
+import 'package:flutter_application_1/Screens/BuyPage.dart'; 
  
 
 class AddressScreen extends StatefulWidget {
